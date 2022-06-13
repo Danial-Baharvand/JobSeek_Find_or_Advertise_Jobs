3de85e4fbcb51e0000b013357923faed9d3019ea
Matt <mweb0015@student.monash.edu>
2022-06-13 20:04:04 +1000

    Nww job interview InvitationsPage

---
+++ src/classes/InvitationsPage.java
@@ -0,0 +1,13 @@
+package classes;
+
+import javax.swing.*;
+
+public class InvitationsPage {
+    private JPanel panel1;
+    private JTable table1;
+    private JRadioButton radioButton1;
+    private JRadioButton radioButton2;
+    private JRadioButton radioButton3;
+    private JButton sendButton;
+    private JTextArea textArea1;
+}
