a7b301f11e75a092c65b3d13a08c803a8fa82595
Matt <mweb0015@student.monash.edu>
2022-06-13 21:52:26 +1000

    New JobApplicationFormPage

---
+++ src/classes/ApplicationFormPage.java
@@ -0,0 +1,16 @@
+package classes;
+
+import javax.swing.*;
+
+public class ApplicationFormPage {
+    private JPanel panel1;
+    private JTextArea textArea1;
+    private JButton viewResumeButton;
+    private JButton updateResumeButton;
+    private ClearingTextField fullNameClearingTextField;
+    private JButton submitButton;
+    private JButton addButton;
+    private JButton deleteButton;
+    private ClearingTextField emailClearingTextField;
+    private ClearingTextField phoneClearingTextField;
+}
